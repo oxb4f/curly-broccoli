6938f5f56d7b341ee4cc533b0453079b0a87a3a4
Artem Lupaschenko <artem.lapa03@gmail.com>
2024-07-10 21:36:39 +0300

    feat: set up basic template of web site

---
+++ frontend/src/main.js
@@ -0,0 +1,12 @@
+import './index.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './app/App.vue'
+
+const app = createApp(App)
+
+app.use(createPinia())
+
+app.mount('#app')
