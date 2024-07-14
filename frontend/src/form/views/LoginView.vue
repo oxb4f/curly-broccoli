f5b1cae6c59be34b66bfdfdf7b2869d99aa61016
Artem Lupaschenko <artem.lapa03@gmail.com>
2024-07-15 00:34:17 +0300

    refactor: create FormItem.vue

--- frontend/src/form/views/LoginView.vue
+++ frontend/src/form/views/LoginView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import BasicForm from '../components/BasicForm.vue'
+import FormItem from '../components/FormItem.vue'
 import ModalWindow from '../components/ModalWindow.vue'
 </script>
 
@@ -8,10 +9,22 @@ import ModalWindow from '../components/ModalWindow.vue'
     <BasicForm>
       <template #header><h1>Вхід</h1></template>
       <template #content>
-        <input type="email" class="form__email-field" placeholder="Ел. пошта" />
-        <span class="form__error"></span>
-        <input type="password" class="form__password-field" placeholder="Пароль" />
-        <span class="form__error"></span>
+        <FormItem
+          field-id="email"
+          field-type="email"
+          field-class="form__email-field"
+          field-placeholder="Ел. пошта"
+          error="Помилка"
+          >Вкажіть електронну пошту:</FormItem
+        >
+        <FormItem
+          field-id="password"
+          field-type="password"
+          field-class="form__password-field"
+          field-placeholder="Пароль"
+          error="Помилка"
+          >Вкажіть пароль:</FormItem
+        >
       </template>
       <template #footer>
         <input type="submit" value="Увійти" class="form__submit-button" />
