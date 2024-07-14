0bad44892732b9b1122c9f0c679224cae4273767
Artem Lupaschenko <artem.lapa03@gmail.com>
2024-07-15 00:34:17 +0300

    refactor: create FormItem.vue

---
+++ frontend/src/form/components/FormItem.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  fieldId: String,
+  fieldType: String,
+  fieldClass: String,
+  fieldPlaceholder: String,
+  error: String
+})
+</script>
+
+<template>
+  <div class="form__item">
+    <label :for="fieldId"><slot></slot></label>
+    <input :id="fieldId" :type="fieldType" :class="fieldClass" :placeholder="fieldPlaceholder" />
+    <span class="form__error">{{ error }}</span>
+  </div>
+</template>
--- frontend/src/form/views/RegisterView.vue
+++ frontend/src/form/views/RegisterView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import BasicForm from '../components/BasicForm.vue'
+import FormItem from '../components/FormItem.vue'
 import ModalWindow from '../components/ModalWindow.vue'
 </script>
 
@@ -8,12 +9,30 @@ import ModalWindow from '../components/ModalWindow.vue'
     <BasicForm>
       <template #header><h1>Реєстрація</h1></template>
       <template #content>
-        <input type="text" class="form__nickname-field" placeholder="Ваше ім'я" />
-        <span class="form__error"></span>
-        <input type="email" class="form__email-field" placeholder="Ел. пошта" />
-        <span class="form__error"></span>
-        <input type="password" class="form__password-field" placeholder="Пароль" />
-        <span class="form__error"></span>
+        <FormItem
+          field-id="nickname"
+          field-type="text"
+          field-class="form__nickname-field"
+          field-placeholder="Ваше ім'я"
+          error="Помилка"
+          >Вкажіть ім'я:</FormItem
+        >
+        <FormItem
+          field-id="email"
+          field-type="email"
+          field-class="form__email-field"
+          field-placeholder="Ел. пошта"
+          error="Помилка"
+          >Вкажіть електронну пошту:</FormItem
+        >
+        <FormItem
+          field-id="password"
+          field-type="password"
+          field-class="form__password-field"
+          field-placeholder="Пароль"
+          error="Помилка"
+          >Вкажіть пароль:</FormItem
+        >
       </template>
       <template #footer>
         <input type="submit" value="Зареєструватись" class="form__submit-button" />
