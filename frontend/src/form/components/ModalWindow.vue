5edb5f1dde634fbe8d083300d50eaf27d5ac67f6
Artem Lupaschenko <artem.lapa03@gmail.com>
2024-07-15 17:55:19 +0300

    feat: add form styles

--- frontend/src/form/components/ModalWindow.vue
+++ frontend/src/form/components/ModalWindow.vue
@@ -2,7 +2,7 @@
 
 <template>
   <Teleport to="body">
-    <div class="modal border-2 border-black">
+    <div class="modal">
       <div class="modal__background"></div>
       <div class="modal__content"><slot></slot></div>
     </div>
