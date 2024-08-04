cd25ab09e6df95901a5aeb638071faa305e9ec6c
oxb4f <off0xbffff@gmail.com>
2024-08-05 01:10:02 +0300

    tests: add hurl tests and improve responses from api

--- backend/src/services/users/create/dto.out.ts
+++ backend/src/services/users/create/dto.out.ts
@@ -2,6 +2,7 @@ export class CreateUserDtoOut {
 	constructor(
 		public readonly id: number,
 		public readonly username: string,
+		public readonly accessId: number,
 		public readonly jwt: { access: string; refresh: string },
 	) {}
 
