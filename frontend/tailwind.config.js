80bcc9d3b70335ce89918154d0541c93f1b0e69b
Artem-Lupaschenko <artem.lapa03@gmail.com>
2024-08-12 22:20:27 +0300

    refactor(app): change site colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,9 +8,9 @@ export default {
         'project-main-color-soft': 'var(--project-main-color-soft)',
         'project-main-color-mute': 'var(--project-main-color-mute)',
         'project-bg-main-color': 'var(--project-bg-main-color)',
+        'project-bg-secondary-color': 'var(--project-bg-secondary-color)',
         'project-text-color': 'var(--project-text-color)',
-        'project-text-color-soft': 'var(--project-text-color-soft)',
-        'project-text-color-mute': 'var(--project-text-color-mute)',
+        'project-text-color-darker': 'var(--project-text-color-darker)',
         'project-text-color-inverted': 'var(--project-text-color-inverted)',
         'project-error-color': 'var(--project-error-color)',
         'project-error-color-soft': 'var(--project-error-color-soft)'
