992b236ffba35b606b1c4e0d07d7cbd40212231a
Artem-Lupaschenko <artem.lapa03@gmail.com>
2024-08-14 12:04:06 +0300

    refactor(authorisation): create media content component

--- frontend/src/app/views/AuthorisationView.vue
+++ frontend/src/app/views/AuthorisationView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterView } from 'vue-router';
 import BaseLogo from '../components/BaseLogo.vue';
+import BaseMediaContent from '../components/BaseMediaContent.vue';
 </script>
 
 <template>
@@ -9,29 +10,6 @@ import BaseLogo from '../components/BaseLogo.vue';
       <header class="authorisation__header"><BaseLogo /></header>
       <main class="authorisation__content"><RouterView></RouterView></main>
     </article>
-    <article class="authorisation__media-content media-content">
-      <div class="media-content__showcase">
-        <img src="../assets/images/iphone-screen.png" alt="iphone-screen" class="w-44" />
-      </div>
-      <div class="media-content__content">
-        <header class="media-content__header"><h1>Download the application</h1></header>
-        <main class="media-content__description description">
-          Our app is made for people who want read their favorite books and share their thoughts
-          with like-minded people
-        </main>
-        <footer class="media-content__footer">
-          <img
-            src="../assets/images/google-play-download.png"
-            class="h-10"
-            alt="Google Play Download"
-          />
-          <img
-            src="../assets/images/microsoft-download.png"
-            class="h-10"
-            alt="Microsoft Download"
-          />
-        </footer>
-      </div>
-    </article>
+    <BaseMediaContent class="authorisation__media-content" />
   </section>
 </template>
