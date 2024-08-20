3fe354ad2c7a68bb7145cae813c4665f41321575
Artem-Lupaschenko <artem.lapa03@gmail.com>
2024-08-20 20:57:56 +0300

    test(api): add user and jwt tests

--- frontend/src/app/services/api/user.js
+++ frontend/src/app/services/api/user.js
@@ -1,28 +1,22 @@
 import api from './api';
-import { setToStorage } from '../storage/storage';
-import { refreshToken } from './jwt';
+import { removeFromStorage, setToStorage } from '../storage/storage';
 
 async function createUser(userData) {
   const response = await api.post('users', userData);
+  setToStorage({ name: 'user', value: response });
   return response;
 }
 
 async function loginUser(userData) {
   const response = await api.post('users/login', userData);
-  response.jwt.access = 'sdfasdf';
-  setToStorage({ name: 'user', value: response });
 
-  await api.post(
-    `accesses/${response.accessId}/refresh`,
-    { refresh: response.jwt.refresh },
-    {
-      headers: {
-        Authorization: `Bearer ${JSON.parse(localStorage.getItem('user'))?.jwt?.access}`
-      }
-    }
-  );
+  setToStorage({ name: 'user', value: response });
 
   return response;
 }
 
-export { createUser, loginUser };
+function logoutUser() {
+  removeFromStorage('user');
+}
+
+export { createUser, loginUser, logoutUser };
